1363cfb28e05d95a065193293ec0e5718ae985bf
kdrbipul <kdrbipul@gmail.com>
2023-08-26 17:05:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <Counter></Counter>
     </div>
+    
   );
 }
+
+function Counter(){
+  
+  const [number, setNumber] = useState(0);
+  const increaseCount = () =>{
+    // console.log("increase");
+    const newCount = number + 1;
+    // console.log(newCount);
+    setNumber (newCount);
+  }
+  const decreaseCount = () =>{
+    // console.log("decrease");
+    setNumber(number - 1);
+  }
+  return (
+    <div className='app-component'>
+      <h1>Count : {number} </h1>
+      <button onClick={increaseCount}>Increase + </button>
+      <button onClick={decreaseCount}>Decrease + </button>
+    </div>
+  );
+  
+}
+
 
 export default App;
